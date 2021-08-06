d4b3697ba18124a5c33e153b96f9c2edfb566f9e
rongwang3 <rongwang3@iflytek.com>
2021-08-06 20:18:53 +0800

    腾讯面试

--- 1. 面试框架/各种实现原生api.js
+++ 1. 面试框架/各种实现原生api.js
@@ -17,7 +17,42 @@ function flatten(arr) {
     },[])
 }
 flatten([1, [2, 3, [4, 5]]])
-
+// 扁平化数组转tree
+function arrayToTree(items) {
+    const result = [];   // 存放结果集
+    const itemMap = {};  // 
+    for (const item of items) {
+      const id = item.id;
+      const pid = item.pid;
+  
+      if (!itemMap[id]) {
+        itemMap[id] = {
+          children: [],
+        }
+      }
+  
+      itemMap[id] = {
+        ...item,
+        children: itemMap[id]['children']
+      }
+  
+      const treeItem =  itemMap[id];
+  
+      if (pid === 0) {
+        result.push(treeItem);
+      } else {
+        if (!itemMap[pid]) {
+          itemMap[pid] = {
+            children: [],
+          }
+        }
+        itemMap[pid].children.push(treeItem)
+      }
+  
+    }
+    return result;
+  }
+  
 
 async await 原理 
 把异步代码同步化
